4fd7d26c266d9522bc1db19fe70e66ae5e80bc0b
lzl131070 <616108722@qq.com>
2019-01-09 20:07:44 +0800

    add a simple middleware

---
+++ middleware/mbbappmiddleware.py
@@ -0,0 +1,17 @@
+import time
+
+from django.http import HttpResponse
+from django.utils.deprecation import  MiddlewareMixin
+
+
+class Mymiddle(MiddlewareMixin):
+    def process_request(self,request):
+        t = time.time()
+        if request.META['REMOTE_ADDR'] not in ipd:
+            ipd[request.META['REMOTE_ADDR']] = t - 1
+        if t - ipd[request.META['REMOTE_ADDR']] < 1:
+
+            time.sleep(5)
+
+        ipd[request.META['REMOTE_ADDR']] = t
+ipd = {}
